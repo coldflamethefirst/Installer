1284417492af7c24c4203084f7ad9ab9cc8156b2
coldflamethefirst <coldflamethefirst@gmail.com>
2014-02-20 16:05:19 +0000

    working a bit
    user has to change .bat file code.

--- installer.bat
+++ installer.bat
@@ -1,7 +1,14 @@
-cd C:\program files
+cd C:\Program Files
 mkdir "installer"
-move C:\Users\%username%\Downloads\Installer\test.txt C:\program files\installer
-move C:\Users\%username%\Downloads\Installer\test.ico C:\program files\installer
-move C:\Users\%username%\Downloads\Installer\test.dll C:\program files\installer
-move C:\Users\%username%\Downloads\Installer\test.exe C:\program files\installer
-move C:\Users\%username%\Downloads\Installer\test.exe.ink C:\Users\%username%\Desktop
+cd C:\Users\%username%\Downloads\Installer
+move *.txt C:\Program Files\installer
+move *.ico C:\Program Files\installer
+move *.dll C:\Program Files\installer
+move *.exe C:\Program Files\installer
+move example.exe.ink C:\Users\%username%\Desktop
+move DIRexample C:\Program Files\installer
+cd C:\ProgramData\Microsoft\Windows\Start Menu\Programs
+mkdir "installer"
+cd C:\Users\%username%\Downloads\Installer
+move startmenuexample.ink C:\ProgramData\Microsoft\Windows\Start Menu\Programs\installer
+move startmenuexample1.ink C:\ProgramData\Microsoft\Windows\Start Menu\Programs\installer
